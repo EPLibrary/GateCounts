72b67af9c3726a5d57e911bbd2b0e03b2478fc83
Andrew Nisbet <anisbet@epl.ca>
2016-09-03 14:51:24 -0600

    Finished getting gates by branch and all branches.

--- gatecountaudit.pl
+++ gatecountaudit.pl
@@ -57,8 +57,8 @@ Usage notes for gatecountaudit.pl.
  -a: Audit the database for broken entries and report don't fix anything. (See -u and -U).
  -d: Turn on debugging.
  -t: Preserve temporary files in $TEMP_DIR.
- -U: Repair all broken entries.
- -u<Gate_ID>: Repair broken entries for a specific gate.
+ -U: Repair all broken entries for all the gates.
+ -u<branch>: Repair broken entries for a specific branches' gates. Processes all the gates at the branch.
  -x: This (help) message.
 
 example:
@@ -140,21 +140,43 @@ sub create_tmp_file( $$ )
 # Returns the gate IDs for a named branch. The branch can be upper or lower case, and need
 # not include the EPL prefix, that is branches are looked up by last three letters of the 
 # branches' code. Example EPLMNA can be submitted as EPLMNA, eplmna, MNA, or mna.
-# param:  string code for the branch.
+# param:  string code for the branch. If the argument is empty, all the branches are returned.
 # return: Array of gate IDs for the branch or an empty list if the branch is misspelled or 
 #         doesn't exist in the database.
 sub get_gate_IDs( $ )
 {
+	# Commands can be issued via system call as below.
+	# echo "select * from gate_info;" | mysql -h mysql.epl.ca -u patroncount -p patroncount --password='somepassword'
+	# Which will produce the following output.
+	# GateId  IpAddress       Branch  Location        Description     LastInCount     LastOutCount    ReverseInOut
+	# 1       10.1.17.135     IDY     MN      3m91200467.epl.ca       1367    395     0
+	# 2       10.1.2.128      CAL     MN      3m91100462.epl.ca       282869  37640   0
+	# ...
 	my $branch = uc( shift );
-	my @ids = ();
+	$branch = `echo $branch | $PIPE -S'c0:-3'`;
+	chomp $branch;
+	my $results = '';
 	if ( ! $branch )
 	{
-		printf STDERR "** requested branch is empty or undefined.\n";
-		return @ids;
+		$results = `echo "select GateId from gate_info;" | mysql -h mysql.epl.ca -u $USER -p $DATABASE --password="$PASSWORD"`;
 	}
-	printf STDERR "Search for branch '$branch' submitted.\n", $branch if ( $opt{'d'} );
-	@ids = `echo "select GateId from gate_info where Branch='$branch';" | mysql -h mysql.epl.ca -u $USER -p $DATABASE --password="$PASSWORD"`;
+	else
+	{
+		$results = `echo "select GateId from gate_info where Branch='$branch';" | mysql -h mysql.epl.ca -u $USER -p $DATABASE --password="$PASSWORD"`;
+	}
+	printf STDERR "Search for branch submitted.\n%s", $results if ( $opt{'d'} );	
+	my @ids = split '\n', $results;
+	# remove the header from the returned table that describes the columns.
+	shift @ids if ( @ids );
 	return @ids;
+}
+
+# Repairs any erroneous entries to some definition of erroneous. TODO define error types fully.
+# param:  Gate id string.
+# return: Number of errors fixed.
+sub repair_gates_by_gate_id( $ )
+{
+	return 0; # Stub for now.
 }
 
 # Audits the database for missing data. Specificially the LANDS table is checked for 
@@ -164,39 +186,7 @@ sub get_gate_IDs( $ )
 # return: none
 sub do_audit()
 {
-	# Commands can be issued via system call as below.
-	# echo "select * from gate_info;" | mysql -h mysql.epl.ca -u patroncount -p ^Ctroncount --password='ub2up@1thbtx'
-	# Which will produce the following output.
-	# GateId  IpAddress       Branch  Location        Description     LastInCount     LastOutCount    ReverseInOut
-	# 1       10.1.17.135     IDY     MN      3m91200467.epl.ca       1367    395     0
-	# 2       10.1.2.128      CAL     MN      3m91100462.epl.ca       282869  37640   0
-	# 3       10.1.2.121      CAL     MN      3m91100463.epl.ca       90      1054    0
-	# 4       10.1.15.112     WOO     MN      3m91100498.epl.ca       26638   409959  0
-	# 5       10.1.15.111     WOO     MN      3m91100499.epl.ca       27669   956     0
-	# 6       10.1.11.129     RIV     MN      3m91100500.epl.ca       28444   10277   0
-	# 7       10.1.11.128     RIV     MN      3m91100501.epl.ca       72658   92311   0
-	# 8       10.1.13.118     SPW     MN      3m91200468.epl.ca       13706   6471    0
-	# 9       10.1.12.122     STR     MN      3m91100497.epl.ca       10576   9466    0
-	# 10      10.1.12.128     STR     MN      3m91200508.epl.ca       230432  80629   0
-	# 11      10.1.9.141      LON     MN      3m91100507.epl.ca       1718    3851    0
-	# 12      10.1.9.126      LON     MN      3m91200506.epl.ca       340526  305469  0
-	# 13      10.2.20.132     MNA     WE      3m91200174.epl.ca       1671243 1656886 0
-	# 14      10.2.20.113     MNA     EA      3m91100057.epl.ca       863980  1106981 0
-	# 15      10.2.20.131     MNA     JU      3m91100175.epl.ca       144554  158122  0
-	# 16      10.2.14.175     WMC     MN      3m91200210.epl.ca       154313  140941  0
-	# 17      10.2.14.183     WMC     MN      3m91200213.epl.ca       4500    3726    0
-	# 18      10.1.16.156     LHL     MN      3m91200170.epl.ca       760033  785363  0
-	# 19      10.2.3.146      CPL     MN      3m91200464.epl.ca       208398  208596  0
-	# 20      10.2.4.133      CSD     MN      3m91100466.epl.ca       19719   362117  0
-	# 21      10.2.4.135      CSD     MN      3m91100465.epl.ca       353101  16997   0
-	# 29      10.2.5.128      CLV     MN      3m91202275.epl.ca       414149  403246  0
-	# 24      10.1.1.160      MLW     EA      3M91200204.epl.ca       810182  809805  0
-	# 25      10.1.18.112     ABB     MN      3m91200461.epl.ca       200281  203712  0
-	# 26      10.1.7.126      JPL     MN      3M91200171.epl.ca       124476  125210  0
-	# 27      10.1.6.122      HIG     MN      3M91202028.epl.ca       74122   77366   0
-	# 28      10.2.8.129      MEA     MN      3M91201585.epl.ca       390296  498313  0
-	# 30      10.2.10.127     WHP     MN      3M9110502.epl.ca        2703    2668    0
-	# 31      10.2.19.123     MCN     MN      3M91100503.epl.ca       16623   16884   0
+	
 }
 
 # Kicks off the setting of various switches.
@@ -209,8 +199,8 @@ sub init
     usage() if ( $opt{'x'} );
 	# Test functions
 	read_password( $PASSWORD_FILE );
-	printf STDERR "Database: %s, Password: ********, Login: %s.\n", $DATABASE, $PASSWORD, $USER if ( $opt{'d'} );
-	my @gate_ids = get_gate_IDs( "EPLMNA" );
+	printf STDERR "Database: %s, Password: ********, Login: %s.\n", $DATABASE, $USER if ( $opt{'d'} );
+	my @gate_ids = get_gate_IDs( "eplclv" );
 	printf STDERR "requested gate ids: %d\n", scalar( @gate_ids );
 }
 
@@ -227,14 +217,27 @@ if ( $opt{'u'} )
 {
 	do_audit() if ( ! $is_audited );
 	$is_audited++;
-	$repairs += repair_entries( $opt{'U'} );
+	my @gates = get_gate_IDs( $opt{'u'} );
+	foreach my $gate ( @gates )
+	{
+		$repairs += repair_gates_by_gate_id( $gate );
+	}
 }
 if ( $opt{'U'} )
 {
 	do_audit() if ( ! $is_audited );
 	$is_audited++;
-	$repairs = repair_entries( $opt{'U'} );
+	my @all_branches = get_all_branches();
+	foreach my $branch ( @all_branches )
+	{
+		my @gates = get_gate_IDs( $branch );
+		foreach my $gate ( @gates )
+		{
+			$repairs += repair_gates_by_gate_id( $gate );
+		}
+	}
 }
+printf STDERR "Total repairs: %d\n", $repairs if ( $opt{'d'} );
 ### code ends
 
 clean_up();
