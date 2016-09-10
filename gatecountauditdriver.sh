b34c00e278a0bdc2223a9e3b0c26033676402275
Andrew Nisbet <anisbet@epl.ca>
2016-09-10 15:01:21 -0600

    Added driver and cronned.

---
+++ gatecountauditdriver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##########################################################
+#
+# Gate Count audit driver for cron.
+#
+##########################################################
+ADDRESSES=anisbet\@epl.ca
+HOME_DIR=/home/ilsdev/projects/gatecountaudit
+if [ -s $HOME_DIR/gatecountaudit.pl ]
+then
+	$HOME_DIR/gatecountaudit.pl -R >log.txt 2>&1
+	cat log.txt | mailx -s"Gate count repair results." $ADDRESSES
+else
+	echo "** error could not find $HOME_DIR/gatecountaudit.pl"
+	exit 99
+fi
+
+#EOF
