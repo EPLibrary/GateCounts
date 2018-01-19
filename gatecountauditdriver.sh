10bf28a860bd8a4cfed5fdf8d2b7f0cc6d0c1417
Andrew Nisbet <anisbet@epl.ca>
2018-01-19 16:55:42 -0700

    Added fully qualified domain name when mailing from ILSDEV1.

--- gatecountauditdriver.sh
+++ gatecountauditdriver.sh
@@ -4,13 +4,13 @@
 # Gate Count audit driver for cron.
 #
 ##########################################################
-ADDRESSES=anisbet\@epl.ca
+ADDRESSES=andrew.nisbet\@epl.ca
 HOME_DIR=/home/ilsdev/projects/gatecountaudit
 cd $HOME_DIR
 if [ -s $HOME_DIR/gatecountaudit.pl ]
 then
 	$HOME_DIR/gatecountaudit.pl -R >log.txt 2>&1
-	cat log.txt | mailx -s"Gate count repair results." $ADDRESSES
+	cat log.txt | mailx -a'From:ilsdev@ilsdev1.epl.ca' -s"Gate count repair results." $ADDRESSES
 else
 	echo "** error could not find $HOME_DIR/gatecountaudit.pl"
 	exit 99
